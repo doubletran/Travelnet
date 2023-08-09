5cac1ae3b5dca2cb2d9c891266b65a3eb8c113e5
blacksea <quetran.tran1009@gmail.com>
2023-08-08 17:24:32 -0700

    adding user

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-/*
-    SETUP
-*/
+//Source: https://github.com/osu-cs340-ecampus/nodejs-starter-app/ 
 
 // Express
 var express = require('express');
@@ -16,10 +14,9 @@ const { query } = require ('express');
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');  
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(__dirname +'/public'));
- 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'));
 
 /*
     ROUTES
@@ -30,11 +27,20 @@ app.get('/', function(req, res)
     });  
 app.get('/users', function (req, res)
     {
-        res.render('users');
+        let query1= `SELECT user_id AS "User ID", user_name AS "User Name", email AS Email, password AS Password 
+        FROM Users;`;
+        db.pool.query(query1, function(error, rows, fields){  
+            res.render('users', {data: rows});
+            })
     });
 app.get('/friendships', function (req, res)
     {
-        let query2 = "SELECT * from Users;";
+        let query2 = `SELECT Friendships.friendship_id AS "Friendship ID", Friendships.start_date AS "Start Date", 
+        Friendships.mutual_friend_ct AS "Mutual Friends Count", user.user_name AS "User 1 Name", 
+        friend.user_name AS "User 2 Name" 
+        FROM Friendships 
+        INNER JOIN Users user ON Friendships.user_id = user.user_id
+        INNER JOIN Users friend ON Friendships.friend_user_id = friend.user_id;`;
         db.pool.query(query2, function(error, rows, fields){  
         res.render('friendships', {data: rows});
         })
@@ -43,14 +49,14 @@ app.get('/friendships', function (req, res)
 app.get('/posts', function (req, res)
     {
         let query1 = `SELECT Posts.post_id AS "Post ID", Posts.content AS "Content", access AS "Access", 
-        user.user_name AS "User Name", user.user_id AS "User ID", GROUP_CONCAT(friend.user_name SEPARATOR', ') AS "Friends Mentioned", 
+        user.user_name AS "User Name", GROUP_CONCAT(friend.user_name SEPARATOR', ') AS "Friends Mentioned", 
         CONCAT(Locations.address, ' ', Locations.city, ' ', 
         Locations.state, ' ', Locations.zip_code, ' ', Locations.country) AS 'Locations Pinned'
-        FROM Posts 
-        LEFT JOIN Posts_has_Friendships ON Posts.post_id = Posts_has_Friendships.post_id
-        INNER JOIN Users user ON user.user_id = Posts.user_id 
-        LEFT JOIN Friendships ON Friendships.friendship_id = Posts_has_Friendships.friendship_id 
-        LEFT JOIN Users friend ON Friendships.friend_user_id = friend.user_id 
+        FROM Posts
+        LEFT JOIN Users user ON user.user_id = Posts.user_id
+        LEFT JOIN Posts_has_Friendships ON Posts_has_Friendships.post_id = Posts.post_id
+        LEFT JOIN Friendships ON Friendships.friendship_id = Posts_has_Friendships.friendship_id
+        LEFT JOIN Users friend ON (CASE WHEN Posts.user_id = Friendships.user_id THEN Friendships.friend_user_id ELSE Friendships.user_id END) = friend.user_id
         LEFT JOIN Locations ON Locations.location_id = Posts.location_id
         GROUP BY Posts.post_id`;
 
@@ -73,6 +79,18 @@ app.get('/posts', function (req, res)
         Locations.state, ' ', Locations.zip_code, ' ', Locations.country) AS 'Locations', location_id
         FROM Locations`;
 
+        let query5 = `SELECT Posts.post_id AS "Post ID", Posts.content AS "Content", access AS "Access", 
+        Posts.user_id AS "User ID", user.user_name AS "User Name", GROUP_CONCAT(friend.user_name SEPARATOR', ') AS "Friends Mentioned", 
+        CONCAT(Locations.address, ' ', Locations.city, ' ', 
+        Locations.state, ' ', Locations.zip_code, ' ', Locations.country) AS 'Locations Pinned'
+        FROM Posts
+        LEFT JOIN Users user ON user.user_id = Posts.user_id
+        LEFT JOIN Posts_has_Friendships ON Posts_has_Friendships.post_id = Posts.post_id
+        LEFT JOIN Friendships ON Friendships.friendship_id = Posts_has_Friendships.friendship_id
+        LEFT JOIN Users friend ON (CASE WHEN Posts.user_id = Friendships.user_id THEN Friendships.friend_user_id ELSE Friendships.user_id END) = friend.user_id
+        LEFT JOIN Locations ON Locations.location_id = Posts.location_id
+        GROUP BY Posts.post_id`;
+
         db.pool.query(query1, function(error, rows, fields){ 
             
             let posts = rows;
@@ -86,9 +104,14 @@ app.get('/posts', function (req, res)
                     let friendships = rows;
                     
                     db.pool.query(query4, function(error, rows, fields){
+                        
+                        let locations = rows;
 
-                        res.render('posts', {posts: posts, users: users, friendships: friendships, locations: rows});
-                    })
+                        db.pool.query(query5, function(error, rows, fields){
+
+                            res.render('posts', {posts: posts, users: users, friendships: friendships, locations: locations, post_for_render: rows});  
+                    })}
+                    )
                 })
                 
             })
@@ -100,7 +123,13 @@ app.get('/locations', function (req, res)
     });
 app.get('/posts-friendships', function (req, res)
     {
-        let query = "SELECT * from Posts_has_Friendships;";
+        let query = `SELECT posts_friendships_id AS "Posts_Friendships ID", Posts.content AS "Post Content", user.user_name AS "User", friend.user_name AS "Friend Mentioned"
+        FROM Posts_has_Friendships
+        INNER JOIN Posts ON Posts_has_Friendships.post_id = Posts.post_id
+        INNER JOIN Friendships ON Posts_has_Friendships.friendship_id = Friendships.friendship_id
+        INNER JOIN Users user ON Posts.user_id = user.user_id
+        INNER JOIN Users friend ON (CASE WHEN Posts.user_id = Friendships.user_id THEN Friendships.friend_user_id ELSE Friendships.user_id END) = friend.user_id
+        ORDER BY Posts_has_Friendships.posts_friendships_id`;
        
         db.pool.query(query, function(error, rows, fields){    // Execute the query
 
@@ -113,86 +142,133 @@ app.get('/posts-friendships', function (req, res)
 app.post('/posts-ajax', function(req, res) 
 {
     let data = req.body;
-
+    let location = data.location;
     // Capture NULL values
-    let location = parseInt(data.location);
-    if (isNaN(location))
+    //let location = parseInt(data.location);
+    
+    if (location === '')
     {
-        location = 'NULL'
+        location = `NULL`;
     }
-
+    
     // First, insert into Posts
     query1 = `INSERT INTO Posts (content, access, user_id, location_id) VALUES 
-    ('${data.content}', '${data.access}', '${data.userID}', '${data.location}')`;
+    ('${data.content}', '${data.access}', '${data.userID}', ${location})`;
+
+
     db.pool.query(query1, function(error, rows, fields) {
         if (error) {
             console.log(error);
             res.sendStatus(400);
         }
-    })
 
-    // Next, for every friend mentioned, insert into the Posts_has_Friendships intersection table.
-    for (let i = 0; i < data.friendList.length; i++) {
-        query2 = `INSERT INTO Posts_has_Friendships (post_id, friendship_id) 
-        VALUES (
-            (SELECT post_id FROM Posts WHERE content = '${data.content}' AND access = '${data.access}' AND user_id = '${data.userID}' AND location_id = '${data.location}'), 
-            (SELECT friendship_id FROM Friendships WHERE (user_id = '${data.userID}' AND friend_user_id = '${data.friendList[i]}') OR (user_id = '${data.friendList[i]}' AND friend_user_id = '${data.userID}'))
-            )`;
-        db.pool.query(query2, function(error, rows, fields){
+        else {
+            // Next, for every friend mentioned, insert into the Posts_has_Friendships intersection table.
+            for (let i = 0; i < data.friendList.length; i++) {
+                let friend = data.friendList[i];
+                query2 = `INSERT INTO Posts_has_Friendships (post_id, friendship_id) VALUES ((SELECT post_id FROM Posts WHERE content = '${data.content}' AND access = '${data.access}' AND user_id = '${data.userID}'), (SELECT friendship_id FROM Friendships WHERE (user_id = '${data.userID}' AND friend_user_id = '${data.friendList[i]}') OR (user_id = '${data.friendList[i]}' AND friend_user_id = '${data.userID}')))`;
+                db.pool.query(query2, function(error, rows, fields){
+                    if (error) {
+                     console.log(error);
+                        res.sendStatus(400);
+                    }
+            })
+            }
+            // Finally, send back the data to be used for appending a row to the Posts table in ajax. 
+            query3 = `SELECT Posts.post_id, Posts.content AS "Content", access AS "Access", 
+            user.user_name, GROUP_CONCAT(friend.user_name SEPARATOR', ') AS "FriendsMentioned", 
+            CONCAT(Locations.address, ' ', Locations.city, ' ', 
+            Locations.state, ' ', Locations.zip_code, ' ', Locations.country) AS 'LocationsPinned'
+            FROM Posts
+            LEFT JOIN Users user ON user.user_id = Posts.user_id
+            LEFT JOIN Posts_has_Friendships ON Posts_has_Friendships.post_id = Posts.post_id
+            LEFT JOIN Friendships ON Friendships.friendship_id = Posts_has_Friendships.friendship_id
+            LEFT JOIN Users friend ON (CASE WHEN Posts.user_id = Friendships.user_id THEN Friendships.friend_user_id ELSE Friendships.user_id END) = friend.user_id
+            LEFT JOIN Locations ON Locations.location_id = Posts.location_id
+            GROUP BY Posts.post_id`;
+
+            db.pool.query(query3, function(error, rows, fields){
             if (error) {
                 console.log(error);
                 res.sendStatus(400);
             }
-        })
-    }
+            else{
+                res.send(rows);
+            }
+            })
+        }});
+    });
 
-    // Finally, send back the data to be used for appending a row to the Posts table in ajax. 
-    query3 = `SELECT Posts.post_id, Posts.content AS "Content", access AS "Access", 
-           user.user_name, GROUP_CONCAT(friend.user_name SEPARATOR', ') AS "FriendsMentioned", 
-           CONCAT(Locations.address, ' ', Locations.city, ' ', 
-           Locations.state, ' ', Locations.zip_code, ' ', Locations.country) AS 'LocationsPinned'
-           FROM Posts 
-           LEFT JOIN Posts_has_Friendships ON Posts.post_id = Posts_has_Friendships.post_id
-           INNER JOIN Users user ON user.user_id = Posts.user_id 
-           LEFT JOIN Friendships ON Friendships.friendship_id = Posts_has_Friendships.friendship_id 
-           LEFT JOIN Users friend ON Friendships.friend_user_id = friend.user_id 
-           LEFT JOIN Locations ON Locations.location_id = Posts.location_id
-           GROUP BY Posts.post_id`;
-    db.pool.query(query3, function(error, rows, fields){
-        if (error) {
-            console.log(error);
-            res.sendStatus(400);
-        }
-        else{
-            res.send(rows);
-        }
-    })
-});
+    
+
+    
 
 app.put('/put-post-ajax', function(req,res,next){
     let data = req.body;
-    console.log(data);
     let location_id = parseInt(data.location_id);
     let content = data.content;
     let access = data.access;
     let friend_ids = data.friend_user_ids;
     let user_id = parseInt(data.user_id);
     let post_id = parseInt(data.post_id);
+
+    if (isNaN(location_id)) {
+        location_id = 'NULL';
+    }
+
+    // Update friends mentioned
+    delete_friends_mentioned = `DELETE FROM Posts_has_Friendships WHERE post_id = ?`;
+
+    db.pool.query(delete_friends_mentioned, post_id, function(error, row_friendship, fields){
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+        }
+        else {
+            for (let i = 0; i < friend_ids.length; i++) {
+                console.log("This line runs!");
+                console.log(i);
+                console.log(friend_ids.length);
+                insert_friends_mentioned = `INSERT INTO Posts_has_Friendships (post_id, friendship_id) VALUES (?, (
+                    SELECT friendship_id FROM Friendships WHERE (user_id = ? AND friend_user_id = ?) OR 
+                    (user_id = ? AND friend_user_id = ?)
+                ))`;
+                db.pool.query(insert_friends_mentioned, [post_id, user_id, parseInt(friend_ids[i]), parseInt(friend_ids[i]), user_id], function(error, row_friendship, fields) {
+                    if (error) {
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                })
+            }
+            update_post_content = `UPDATE Posts SET content = ?, access = ?, location_id = ? WHERE post_id = ?`
+            db.pool.query(update_post_content, [content, access, location_id, post_id], function(error, row_friendship, fields) {
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    
+                }
+            })
+        }
+    })
+});
    // let select_friendship = `SELECT friendship_id FROM Friendships 
    // WHERE user_id = ? AND friend_user_id = ?`;
-   for (let i = 0; i < friend_ids.length; i++) {
-        let update_post = `UPDATE Posts_has_Friendships SET friendship_id = (SELECT friendship_id FROM Friendships WHERE (user_id = ? 
-            AND friend_user_id = ? ) OR (user_id = ? AND friend_user_id = ?)) WHERE post_id = ?;`;
-       // let update_post = `(SELECT friendship_id FROM Friendships WHERE (user_id = '${data.user_id}' AND friend_user_id = '${data.friend_ids[i]}') OR (user_id = '${data.friend_ids[i]}' AND friend_user_id = '${data.user_id}'))`
-        db.pool.query(update_post ,[user_id, parseInt(data.friend_user_ids[i]), parseInt(data.friend_user_ids[i]), user_id, post_id], function(error, row_friendship, fields){
-            if (error) {
+//    for (let i = 0; i < friend_ids.length; i++) {
+//         let update_post = `UPDATE Posts_has_Friendships SET friendship_id = (SELECT friendship_id FROM Friendships WHERE (user_id = ? 
+//             AND friend_user_id = ? ) OR (user_id = ? AND friend_user_id = ?)) WHERE post_id = ?`;
+//        // let update_post = `(SELECT friendship_id FROM Friendships WHERE (user_id = '${data.user_id}' AND friend_user_id = '${data.friend_ids[i]}') OR (user_id = '${data.friend_ids[i]}' AND friend_user_id = '${data.user_id}'))`
+//         db.pool.query(update_post ,[user_id, parseInt(data.friend_user_ids[i]), parseInt(data.friend_user_ids[i]), user_id, post_id], function(error, row_friendship, fields){
+//             if (error) {
   
-                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
-                console.log(error);
-                res.sendStatus(400);
-                }
-        })
-   }});
+//                 // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+//                 console.log(error);
+//                 res.sendStatus(400);
+//                 }
+//             else {console.log("successfully!")}
+//         })
+//    }});
+    
 
 app.delete('/delete-post-ajax/', function(req,res,next){                                                                
     let data = req.body;
@@ -223,14 +299,51 @@ app.delete('/delete-post-ajax/', function(req,res,next){
         
                     else
                     {
-                        // Run the second query
                        res.sendStatus(204);
                     }
-              }
+              })
             }
   })});
   
+
+
   /*
+  ADD USER
+  */
+
+  app.post('/add-user-ajax', function(req, res, next){
+    
+    let data = req.body;
+    console.log(data);
+    let addUserQuery = `INSERT INTO Users (user_name, email, password) VALUES ('${data.username}', '${data.email}', '${data.password}');`;
+    let readNewUserQuery = `SELECT * FROM Users WHERE user_name = ?;`;
+    db.pool.query(addUserQuery, function (error, row, fields){
+        if (error){
+            console.log(error.errno);
+            res.sendStatus(400);
+            if (error.errno == 1062){
+     
+            }
+            
+
+        }
+        else{
+            db.pool.query(readNewUserQuery, [data.username], function(error, row, fields){
+                if (error){
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                else{
+                    console.log(row);
+                    res.send(row);
+                }
+            })
+
+        }
+    })
+  })
+  /*
+
     LISTENER
 */
 app.listen(PORT, function(){
---
+++ public/js/add_user.js
@@ -0,0 +1,91 @@
+//Source: https://github.com/osu-cs340-ecampus/nodejs-starter-app/ 
+let addUserForm = document.getElementById('addUser');
+
+// Modify the objects we need
+addUserForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputUsername = document.getElementById("userName");
+    let inputEmail = document.getElementById("email");
+    let inputPassword = document.getElementById("password");
+
+    // Get the values from the form fields
+    let usernameValue = inputUsername.value;
+    let emailValue = inputEmail.value;
+    let passwordValue = inputPassword.value;
+	
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        username: usernameValue,
+        email: emailValue,
+        password: passwordValue,
+
+    }
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", '/add-user-ajax', true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputUserID.value = '';
+            inputFriendList.value = '';
+            inputContent.value = '';
+            inputLocation.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+addRowToTable = (data) => {
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("user-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+    console.log(newRow);
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let userIDCell = document.createElement("TD");
+    let usernameCell = document.createElement("TD");
+    let passwordCell = document.createElement("TD");
+    let emailCell = document.createElement("TD");
+
+    // // Fill the cells with correct data
+    userIDCell.innerText = newRow.user_id;
+    usernameCell.innerText = newRow.user_name;
+    passwordCell.innerText = newRow.password;
+    emailCell.innerHTML = newRow.email;
+
+    // // Add the cells to the row 
+    row.appendChild(userIDCell);
+    row.appendChild(usernameCell);
+    row.appendChild(emailCell);
+    row.appendChild(passwordCell);
+    
+    // // Add the row to the table
+    currentTable.appendChild(row);
+}
